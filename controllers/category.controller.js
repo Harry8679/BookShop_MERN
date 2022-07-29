c8758d0dbf985d417c5396267559f8623374bb3f
Harry EMARH <emarh.harry.code@gmail.com>
2022-07-29 10:33:55 +0200

    Get a Category By ID

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -11,3 +11,19 @@ exports.create = (req, res) => {
         res.json({ data });
     });
 }
+
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if (err || !category) {
+            return res.status(400).json({
+                error: 'La catégorie n\'existe pas !'
+            });
+        }
+        req.category = category;
+        next();
+    });
+}
+
+exports.read = (req, res) => {
+    return res.json(req.category);
+}
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/category.controller');
+const { create, categoryById, read } = require('../controllers/category.controller');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth.controller');
 const { userById } = require('../controllers/user.controller');
 
+router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
 
+router.param('categoryId', categoryById);
 router.param('userId', userById);
 
 module.exports = router;

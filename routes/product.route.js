49ba0763ec9da6041b5684ee2a54ad2a1df6057d
Harry EMARH <emarh.harry.code@gmail.com>
2022-08-08 11:14:37 +0200

    Photo and Search routes

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove, update, list, listRelated, listCategories } = require('../controllers/product.controller');
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo } = require('../controllers/product.controller');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth.controller');
 const { userById } = require('../controllers/user.controller');
 
@@ -13,6 +13,9 @@ router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update
 router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
 router.get('/products/categories', listCategories);
+
+router.get('/products/by/search', listBySearch);
+router.get('/product/photo/:productId', photo);
 
 router.param('userId', userById);
 router.param('productId', productById);
